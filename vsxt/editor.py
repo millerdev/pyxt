402c6bc060c00ea77925a4aca1a480b1e9cdb7f3
Daniel Miller <millerdev@gmail.com>
2020-12-12 23:12:55 -0500

    More complete editor implementation

--- vsxt/editor.py
+++ vsxt/editor.py
@@ -1,28 +1,27 @@
 from os.path import dirname, expanduser, isabs
 
 from .jsproxy import JSProxy
+from .util import cached_property
 
 
 class Editor:
     def __init__(self, server):
         self.proxy = JSProxy(server)
 
-    @property
+    @cached_property
     async def file_path(self):
         return await self.proxy.window.activeTextEditor.document.uri.fsPath
 
-    @property
+    @cached_property
     async def project_path(self):
-        raise NotImplementedError
+        folders = await self.proxy.workspace.workspaceFolders
+        return folders[0] if folders else expanduser("~")
 
-    @property
+    @cached_property
     async def dirname(self):
         path = await self.file_path
         if path:
             current_dir = dirname(path)
             if current_dir and isabs(current_dir):
                 return current_dir
-        folders = await self.proxy.workspace.workspaceFolders
-        if folders:
-            return folders[0]
-        return expanduser("~")
+        return await self.project_path
--- vsxt/util.py
+++ vsxt/util.py
@@ -1,4 +1,5 @@
 import os
+from asyncio import ensure_future, iscoroutinefunction
 
 
 def user_path(path, home=os.path.expanduser('~')):
@@ -6,3 +7,19 @@ def user_path(path, home=os.path.expanduser('~')):
     if os.path.normpath(path).startswith(home + os.sep):
         path = '~' + os.path.normpath(path)[len(home):]
     return path
+
+
+class cached_property:
+
+    def __init__(self, func):
+        self.__doc__ = func.__doc__
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        value = self.func(obj)
+        if iscoroutinefunction(self.func):
+            value = ensure_future(value)
+        obj.__dict__[self.func.__name__] = value
+        return value
