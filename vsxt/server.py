7768f8001c7e3bae96e4c0d1b67266bbf4b6b3d5
Daniel Miller <millerdev@gmail.com>
2020-12-21 12:34:42 -0500

    input_value -> argstr
    Disambiguate `input_value` and `argstr` so they have consistent meanings in all contexts.

--- vsxt/server.py
+++ vsxt/server.py
@@ -24,33 +24,34 @@ async def do_command(server: XTServer, params):
     input_value, = value, = params
     if not input_value:
         return result(sorted(cmd.REGISTRY), "", offset=0)
-    command, input_value, offset = parse_command(input_value)
+    command, argstr, offset = parse_command(input_value)
     if not command:
-        return error(f"Unknown command: {input_value!r}")
+        return error(f"Unknown command: {argstr!r}")
     editor = Editor(server)
     parser = await command.parser.with_context(editor)
-    args = await parser.parse(input_value)
+    args = await parser.parse(argstr)
     try:
         return await command(editor, args)
     except cmd.Incomplete as err:
         value += err.addchars
-        input_value += err.addchars
-    return await _get_completions(value, parser, input_value, offset)
+        argstr += err.addchars
+    return await _get_completions(value, parser, argstr, offset)
 
 
 @xt_command
 async def get_completions(server: XTServer, params):
     input_value, = params
     if input_value:
-        command, input_value, offset = parse_command(input_value)
+        command, argstr, offset = parse_command(input_value)
     else:
+        argstr = ""
         offset = None
         command = None
     if not command:
-        return result(sorted(cmd.REGISTRY), input_value, offset=0)
+        return result(sorted(cmd.REGISTRY), argstr, offset=0)
     editor = Editor(server)
     parser = await command.parser.with_context(editor)
-    return await _get_completions(params[0], parser, input_value, offset)
+    return await _get_completions(input_value, parser, argstr, offset)
 
 
 def parse_command(input_value):
@@ -59,21 +60,22 @@ def parse_command(input_value):
     name = parts[0]
     if len(parts) > 1:
         assert len(parts) == 2, parts
-        input_value = parts[1]
+        argstr = parts[1]
     else:
-        input_value = ""
+        argstr = ""
     COMMANDS = cmd.REGISTRY
     if name in COMMANDS:
-        return COMMANDS[name], input_value, len(name) + 1
+        return COMMANDS[name], argstr, len(name) + 1
     return None, name, 0
 
 
-async def _get_completions(value, parser, input_value, offset):
-    items = await parser.get_completions(input_value)
+async def _get_completions(input_value, parser, argstr, offset):
+    input_value = input_value.ljust(offset)
+    items = await parser.get_completions(argstr)
     if not items:
-        placeholder = await parser.get_placeholder(input_value)
+        placeholder = await parser.get_placeholder(argstr)
         if placeholder:
-            command = value.ljust(offset) + placeholder
+            command = input_value + placeholder
             items.append({"label": "", "description": command})
-            items.offset = len(input_value)
-    return result(items, value.ljust(offset), offset=offset + items.offset)
+            items.offset = len(argstr)
+    return result(items, input_value, offset=offset + items.offset)
